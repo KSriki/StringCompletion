ca358f30eead392b8a10a224efd3995124718ee4
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2017-06-19 12:01:22 -0400

    String Completetion with escape sequences

---
+++ src/PetQuestions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+  
+ public class PetQuestions
+ {
+     public static void main( String[] args )
+    {
+    	 
+    	 //String Completion
+        /*String name;
+        String breed;
+        int age;
+
+        System.out.print( "Greetings. What is your pet's name? " );
+        Scanner keyboard = new Scanner(System.in);
+        name = keyboard.next();
+ 
+        System.out.print( "What kind of animal is " + name + "? " );
+        breed = keyboard.next();
+        System.out.print( "How old is " + name + "? ");
+        age = keyboard.nextInt();
+        System.out.println( name + " is your " + breed + " and it is " + age );
+         */
+    	 
+    	 //Escape Sequences
+    	 System.out.println("Cost\t  Quantity  Total");
+    	 System.out.println("$1,000.00  4       $4,000.00");
+    	 System.out.println("$   50.00  8         $400.00");
+    	 System.out.println("_________________________");
+    	 System.out.println("TOTAL:     12\t   $4,400.00");
+    	 
+    	 
+    	 
+     }
+ }
